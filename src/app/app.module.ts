47363b96d82c4d699d74e87845d42e36e70ad93d
Ravi448 <jarvicephp@gmail.com>
2017-10-02 11:59:45 +0530

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import {Geolocation} from '@ionic-native/geolocation';
+// import {Storage,IonicStorageModule} from '@ionic/Storage';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -18,6 +20,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    // IonicStorageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -28,7 +31,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Geolocation
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,107 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController, LoadingController } from 'ionic-angular';
 
+import {Geolocation} from '@ionic-native/geolocation';
+
+declare var google;
+var latLng;
+var markersArray = [];
+var loader;
+var geocoder;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild('map') mapChild:ElementRef;
+  @ViewChild('input') inputChild:HTMLInputElement;
 
-  constructor(public navCtrl: NavController) {
+  latitude:any;
+  longitude:any;
+  maps:any;
 
+  LastLat:any;
+  LastLng:any;
+  result:any;
+
+  constructor(
+      public navCtrl: NavController,
+      private loadingCtrl:LoadingController,
+      private geolocation:Geolocation,
+    ) {
+      geocoder = new google.maps.Geocoder();
+      loader = this.loadingCtrl.create({
+        spinner:'dots'
+      });
+      loader.present();
+      this.geolocation.getCurrentPosition({enableHighAccuracy:true}).then((res)=>{
+        loader.dismiss();
+        this.latitude = res.coords.latitude;
+        this.longitude = res.coords.longitude;
+        this.renderMap();
+      });
+
+      setInterval(()=>{
+        console.log(this.result);
+      },2000);
+  }
+
+  ionViewDidLoad(){
+    // this.renderMap();
+  }
+
+  renderMap() {
+    latLng = new google.maps.LatLng(this.latitude,this.longitude);
+    this.setCenter(latLng);
+    this.dropMarker(latLng);
+    this.autocomplete();
+
+  }
+
+  autocomplete(){
+    let input = document.getElementById('input');
+    console.log(input);
+    let autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.bindTo('bounds', this.maps);
+    google.maps.event.addListener(autocomplete, 'place_changed', () => {
+      const place = autocomplete.getPlace();
+      if (place.geometry) {
+        console.log(place);
+        this.setCenter({lat:place.geometry.location.lat(),lng:place.geometry.location.lng()});
+        this.dropMarker({lat:place.geometry.location.lat(),lng:place.geometry.location.lng()});
+      }else{
+        alert('Sorry, Google could not find any place with '+input);
+      }
+    });
+  }
+
+  setCenter(latLng){
+    let mapOptions = {
+      center:latLng,
+      zoom:17,
+      mapType:google.maps.MapTypeId.ROADMAP
+    }
+    this.maps = new google.maps.Map(this.mapChild.nativeElement,mapOptions);
+  }
+
+  dropMarker(latLng){
+    let markers = new google.maps.Marker({
+      map:this.maps,
+      position:latLng,
+      animation: google.maps.Animation.DROP,
+      draggable:true,
+      icon:'assets/icon/map-pin.png'
+    });
+    markersArray.push(markers);
+    this.lastLatLng(markers);
+  }
+
+  lastLatLng(marker){
+    google.maps.event.addListener(marker, 'dragend', () =>{
+      geocoder.geocode({latLng: marker.getPosition()},(responses)=>{
+        this.result =  responses[0];
+      });
+    });
   }
 
 }
